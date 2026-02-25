98f09cee1eca324fd8624d085af675d792da9d5d
Spatulox <M78stormtrooper@laposte.net>
2026-02-25 15:09:16 +0100

    Initial commit

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig([
+    {
+        entry: ['src/index.ts'],                    // VOTRE index.ts
+        format: ['cjs', 'esm'],                     // CommonJS + ESM
+        dts: true,                                  // Génère .d.ts
+        sourcemap: false,                           // Pas de .map
+        clean: true,                                // Nettoie dist/
+        outDir: 'dist',
+    }
+])
