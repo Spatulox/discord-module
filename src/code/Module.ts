80c440a48cef87d9be3fddb4dddf40b9153968f0
Spatulox <M78stormtrooper@laposte.net>
2026-02-25 18:52:07 +0100

    Update Module.ts
    Adds a standardized method for generating Discord UI components for module management. Encapsulates the module's enabled state and introduces a method to toggle its status.

--- src/code/Module.ts
+++ src/code/Module.ts
@@ -1,14 +1,23 @@
-import {ClientEvents} from 'discord.js';
+import {ButtonBuilder, ButtonStyle, ClientEvents, SectionBuilder, TextDisplayBuilder} from 'discord.js';
 
 export type ModuleEventsMap = Partial<Record<keyof ClientEvents, (...args: any[]) => any>>;
 
 export abstract class Module     {
     public abstract name: string;
     public abstract description: string;
-    public enabled = false;
+    private _enabled = false;
 
     public abstract get events(): ModuleEventsMap;
 
-    enable() { this.enabled = true; }
-    disable() { this.enabled = false; }
+    public createModuleUI(): SectionBuilder {
+        return new SectionBuilder()
+            .addTextDisplayComponents(new TextDisplayBuilder().setContent(`# ${this.name}`))
+            .addTextDisplayComponents(new TextDisplayBuilder().setContent(`${this.description}`))
+            .setButtonAccessory(new ButtonBuilder().setLabel(this.enabled ? "Disabled" : "Enable").setCustomId(`toggle_${this.name.toLowerCase()}`).setStyle(this.enabled ? ButtonStyle.Danger : ButtonStyle.Success))
+    }
+
+    get enabled(): boolean {return this._enabled}
+    toogleEnabled() {this._enabled = !this._enabled}
+    enable() { this._enabled = true; }
+    disable() { this._enabled = false; }
 };
