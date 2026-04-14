2616ed6beba56df4bbebe14703c4c981381f61c0
Spatulox <M78stormtrooper@laposte.net>
2026-04-14 15:03:16 +0200

    Export the ModuleUI class...

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export {Module, ModuleEventsMap} from "./code/Module";
 export {MultiModule} from "./code/MultiModule";
 export {ModuleManager} from "./code/ModuleManager";
 export {ModuleRegistry} from "./code/ModuleRegistry";
+export {ModuleUI} from "./code/ModuleUI";
 export {InteractionsManager, InteractionType, InteractionMatchType} from "./code/InteractionsManager";
